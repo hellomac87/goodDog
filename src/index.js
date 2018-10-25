0e3f5e7b266ee3e16e5298fcaeb880d3c5ca7f07
ohahthink <ohahthink@gmail.com>
2018-10-25 14:23:43 +0900

    FEAT: create state

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-const randomNumber = [];
+// 상태관리객체
+const state = {
+  randomNumber : [],
+
+};
 
 // 랜덤으로 중복되지 않는 세자리 숫자 생성
 
@@ -6,16 +10,16 @@ const randomNumber = [];
 for (let i = 0; i<3; i++) {
   // 0-9까지 정수를 랜덤으로 생성
   let random = Math.floor(Math.random() * 10);
-  // randomNumber가 random을 가지고 있을 경우 루프를 한번 더 순회한다
-  if (randomNumber.includes(random)) {
+  // state.randomNumber가 random을 가지고 있을 경우 루프를 한번 더 순회한다
+  if (state.randomNumber.includes(random)) {
     i--
   } else {
-    // randomNumber가 random을 가지고 있지 않을 경우 생성한 수를 배열에 추가한다
-    randomNumber.push(random)
+    // state.randomNumber가 random을 가지고 있지 않을 경우 생성한 수를 배열에 추가한다
+    state.randomNumber.push(random)
   }
 }
 
 // test
-document.getElementById('test').textContent = randomNumber
+document.getElementById('test').textContent = state.randomNumber
 
 

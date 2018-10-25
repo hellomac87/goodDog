5bd12a931e6ebbb72678bb9116d6ebadb30cd213
ohahthink <ohahthink@gmail.com>
2018-10-25 13:00:03 +0900

    FEAT: random number create

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-document.querySelector('h1').addEventListener('click', e => {
-  alert('Hell World!');
-});
+const randomNumber = [];
+
+// 랜덤으로 중복되지 않는 세자리 숫자 생성
+
+
+for (let i = 0; i<3; i++) {
+  // 0-9까지 정수를 랜덤으로 생성
+  let random = Math.floor(Math.random() * 10);
+  // randomNumber가 random을 가지고 있을 경우 루프를 한번 더 순회한다
+  if (randomNumber.includes(random)) {
+    i--
+  } else {
+    // randomNumber가 random을 가지고 있지 않을 경우 생성한 수를 배열에 추가한다
+    randomNumber.push(random)
+  }
+}
+
+// test
+document.getElementById('test').textContent = randomNumber
+
+

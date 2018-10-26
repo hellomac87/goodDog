c1b234e1c669df110dfaacb86c46c75a5a8ec192
hellomac87 <hellomac87@gmail.com>
2018-10-26 10:54:39 +0900

    Feat: draw game state at list, function refactoring

--- src/index.js
+++ src/index.js
@@ -7,24 +7,30 @@ const state = {
   nyam : 0,
 };
 
+function init(){
+  // 게임 상태 및 화면 초기화 함수
+  createRandomNumber(); // 랜덤으로 중복되지 않는 세자리 숫자 생성
+}
 
-// 랜덤으로 중복되지 않는 세자리 숫자 생성
-for (let i = 0; i<3; i++) {
-  // 0-9까지 정수를 랜덤으로 생성
-  let random = Math.floor(Math.random() * 10);
-  // state.randomNumber가 random을 가지고 있을 경우 루프를 한번 더 순회한다
-  if (state.randomNumber.includes(random)) {
-    i--
-  } else {
-    // state.randomNumber가 random을 가지고 있지 않을 경우 생성한 수를 배열에 추가한다
-    state.randomNumber.push(random)
+function createRandomNumber(){
+  // 랜덤으로 중복되지 않는 세자리 숫자 생성
+  for (let i = 0; i < 3; i++) {
+    // 0-9까지 정수를 랜덤으로 생성
+    let random = Math.floor(Math.random() * 10);
+    // state.randomNumber가 random을 가지고 있을 경우 루프를 한번 더 순회한다
+    if (state.randomNumber.includes(random)) {
+      i--
+    } else {
+      // state.randomNumber가 random을 가지고 있지 않을 경우 생성한 수를 배열에 추가한다
+      state.randomNumber.push(random)
+    }
   }
+
+  // text
+  document.getElementById('test').textContent = state.randomNumber;
 }
 
 // 상태업데이트
-document.getElementById('test').textContent = state.randomNumber;
-
-
 document.querySelector('.game-try-button').addEventListener('click', e => {
   e.preventDefault();
 
@@ -56,7 +62,7 @@ function drawList() {
         <input value="${state.userInput[0]}" type="text" class="game-input-1" maxlength="1" readonly>
         <input value="${state.userInput[1]}" type="text" class="game-input-10" maxlength="1" readonly>
         <input value="${state.userInput[2]}" type="text" class="game-input-100" maxlength="1" readonly>
-        1멍 <span>1냠</span>
+        <span class="mung">${state.mung} 멍</span> <span class="nyam">${state.nyam} 냠</span>
       </li>`;
   document.querySelector('.game-try-list').insertAdjacentHTML("beforeend", listtemplete);
 }
@@ -88,3 +94,4 @@ function isWin() {
 
 }
 
+init();
